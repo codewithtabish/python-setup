0b6c73147f33ff9ea812d2609d6d642fd9d786e5
codewithzain099 <zainiikhan099@gmail.com>
2024-11-23 11:12:00 -0800

    addedtwo more items  in index file of constants

--- constants/index.py
+++ constants/index.py
@@ -2,7 +2,10 @@
 product_list = [
         {"id": 1, "name": "Product 1", "price": 10.99},
         {"id": 2, "name": "Product 2", "price": 20.99},
-        {"id": 3, "name": "Product 3", "price": 30.99}
+        {"id": 3, "name": "Product 3", "price": 30.99},
+        {"id": 5, "name": "Dell Laptop", "price": 40.99},
+        {"id": 6, "name": "Dell Watch", "price": 40.99},
+        {"id": 7, "name": "Dell EarPhone", "price": 40.99},
     ]
 
 mobiles_list= [
