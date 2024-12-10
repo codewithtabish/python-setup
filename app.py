82ff7e0f2faec03e9ee8e3606dcbaf8931325cd9
codewithzain099 <zainiikhan099@gmail.com>
2024-12-10 00:57:42 -0800

    added port var in app file

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def home():
 
 if __name__ == "__main__":
 
-    app.run(debug=True, host='0.0.0.0', port=PORT)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT",5000)))
 
 # cost_analyzer\Scripts\activate
         # download_dir = r"C:\Users\Mark 1\Downloads"
